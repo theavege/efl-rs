71e8ed70a8edb6a7795e2bca5ee1cafee01a0f5d
Artem V. Ageev <artem.v.ageev@gmail.com>
2026-04-27 06:10:54 +0300

    fix ci

--- .github/workflows/make.sh
+++ .github/workflows/make.sh
@@ -1,8 +1,11 @@
 set -euo pipefail
 source '/etc/os-release'
 case ${ID:?} in
-    debian | ubuntu) sudo apt-get install -y libefl-all-dev ;;
-    fedora | alma) sudo apt-get install -y efl-devel ;;
+    debian | ubuntu) sudo bash -c '
+        apt-get update
+        apt-get install -y libefl-all-dev
+    ';;
+    fedora | alma) sudo dnf install -y efl-devel ;;
 esac 1> /dev/null
 cargo clippy --quiet --example simple
 cargo build --release --example simple
