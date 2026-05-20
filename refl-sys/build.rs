756d6d32a4f217f050da108f849437617060b36e
Artem Ageev <artem.v.ageev@gmail.com>
2026-05-20 22:39:11 +0300

    small fix

--- refl-sys/build.rs
+++ refl-sys/build.rs
@@ -24,14 +24,15 @@ impl ParseCallbacks for MacroCallback {
     }
 }
 
-fn main() {
-    let mut cflags = Vec::new();
+#[cfg(target_os = "linux")]
+fn compile() -> Vec<String> {
     let library = "elementary";
     println!("cargo:rustc-link-lib=dylib={library}");
+    let mut includes = Vec::new();
     match pkg_config::probe_library(library) {
         Ok(lib) => {
             for dir in lib.include_paths {
-                cflags.push(format!("-I{}", dir.display()));
+                includes.push(format!("-I{}", dir.display()));
             }
         }
         Err(e) => {
@@ -39,9 +40,13 @@ fn main() {
             std::process::exit(1);
         }
     }
+    includes
+}
+
+fn main() {
     bindgen::Builder::default()
-        .header("wrapper.h")
-        .clang_args(&cflags)
+        .header("src/wrapper.h")
+        .clang_args(compile())
         .parse_callbacks(Box::new(MacroCallback::default()))
         .generate()
         .expect("Unable to generate bindings")
