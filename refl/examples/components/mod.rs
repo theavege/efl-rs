d5965ea7324ae30239297f6e6fc65165a839cef0
Artem V. Ageev <artem.v.ageev@gmail.com>
2026-06-02 20:35:25 +0300

    Update components mod.rs to include Photo and Image

--- refl/examples/components/mod.rs
+++ refl/examples/components/mod.rs
@@ -2,12 +2,14 @@ mod booker;
 mod calculator;
 mod converter;
 mod dialect;
+mod image;
 mod niccalc;
+mod photo;
 mod rangers;
 mod selectors;
 mod sudoku;
 
 pub use {
-    booker::Booker, calculator::Calc, converter::Converter, dialect::Dialect, niccalc::NicCalc,
-    rangers::Ranger, selectors::Selector, sudoku::Sudoku,
+    booker::Booker, calculator::Calc, converter::Converter, dialect::Dialect, image::ImageExample,
+    niccalc::NicCalc, photo::PhotoExample, rangers::Ranger, selectors::Selector, sudoku::Sudoku,
 };
