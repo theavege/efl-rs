f5bfb4fddf2fcc9d5dcc16959c4ba58ba1e5d823
Artem V. Ageev <artem.v.ageev@gmail.com>
2026-06-02 20:35:38 +0300

    Add Image example

---
+++ refl/examples/image_example.rs
@@ -0,0 +1,30 @@
+use refl::prelude::*;
+
+#[derive(Default)]
+pub struct ImageApp {}
+
+impl Component for ImageApp {
+    type Event = ();
+    type State = ();
+    
+    fn update(&self, _model: &Self::State) {}
+    
+    fn handle(_msg: Self::Event, _model: &mut Self::State, _: Sender<Self::Event>) -> bool {
+        true
+    }
+    
+    fn view(&mut self, prt: &impl ContainerExt, _sender: Sender<Self::Event>) {
+        refl::Box::new(prt).with_vertical(true).inside(|prt| {
+            refl::Label::new(prt).with_text("Image Widget Example");
+            
+            // Create an image widget
+            refl::Image::new(prt)
+                .with_size(300, 300)
+                .with_mmap(false);
+        });
+    }
+}
+
+fn main() {
+    ImageApp::run("Image Example");
+}
