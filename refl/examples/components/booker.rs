451e180e57e30c3d5a81b4487e7ff43f99b7c82c
Artem V. Ageev <artem.v.ageev@gmail.com>
2026-06-03 16:34:23 +0300

    Add EFL Index, Image, Video and Prefs bindings (#21)
    * Add bindings for EFL media and prefs widgets

--- refl/examples/components/booker.rs
+++ refl/examples/components/booker.rs
@@ -23,54 +23,61 @@ impl Component for Booker {
         true
     }
     fn view(&mut self, prt: &impl ContainerExt, sender: Sender<Self::Event>) {
-        refl::Box::new(prt).inside(|prt| {
-            refl::Panes::new(prt).inside(|prt| {
-                refl::Label::new(prt).with_text("Flight");
-                refl::FlipSelector::new(prt)
-                    .with_items(&["One-way", "Return"])
-                    .with_selected({
-                        let sender = sender.clone();
-                        move |wgt| {
-                            if wgt.focus() {
-                                sender.send(Msg::Flight(wgt.value())).unwrap();
+        refl::Box::new(prt).with_homogeneous(true).inside(|prt| {
+            refl::Panes::new(prt)
+                .with_fixed_size(0.6, 0.4)
+                .inside(|prt| {
+                    refl::Label::new(prt).with_text("Flight");
+                    refl::SegmentControl::new(prt)
+                        .with_items(&["One-way", "Return"])
+                        .with_value(0)
+                        .with_changed({
+                            let sender = sender.clone();
+                            move |wgt| {
+                                if wgt.focus() {
+                                    sender.send(Msg::Flight(wgt.value())).unwrap();
+                                }
                             }
-                        }
-                    });
-            });
-            refl::Panes::new(prt).inside(|prt| {
-                refl::Label::new(prt).with_text("Departure data");
-                refl::Entry::new(prt)
-                    .with_clicked(move |wgt| {
-                        refl::Notify::new(&wgt).inside(|ntf| {
-                            let _ = refl::Calendar::new(ntf);
                         });
-                    })
-                    .with_changed({
-                        let _sender = sender.clone();
-                        move |wgt| {
-                            if wgt.focus() {
-                                let _value = wgt.value().parse::<f64>().unwrap_or_default();
+                });
+            refl::Panes::new(prt)
+                .with_fixed_size(0.6, 0.4)
+                .inside(|prt| {
+                    refl::Label::new(prt).with_text("Departure data");
+                    refl::Entry::new(prt)
+                        .with_clicked(move |wgt| {
+                            refl::Notify::new(&wgt).inside(|ntf| {
+                                let _ = refl::Calendar::new(ntf);
+                            });
+                        })
+                        .with_changed({
+                            let _sender = sender.clone();
+                            move |wgt| {
+                                if wgt.focus() {
+                                    let _value = wgt.value().parse::<f64>().unwrap_or_default();
+                                }
                             }
-                        }
-                    });
-            });
-            refl::Panes::new(prt).inside(|prt| {
-                refl::Label::new(prt).with_text("Return data");
-                refl::Entry::new(prt)
-                    .with_clicked(move |wgt| {
-                        refl::Notify::new(&wgt).inside(|ntf| {
-                            let _ = &refl::Calendar::new(ntf);
                         });
-                    })
-                    .with_changed({
-                        let _sender = sender.clone();
-                        move |wgt| {
-                            if wgt.focus() {
-                                let _value = wgt.value().parse::<f64>().unwrap_or_default();
+                });
+            refl::Panes::new(prt)
+                .with_fixed_size(0.6, 0.4)
+                .inside(|prt| {
+                    refl::Label::new(prt).with_text("Return data");
+                    refl::Entry::new(prt)
+                        .with_clicked(move |wgt| {
+                            refl::Notify::new(&wgt).inside(|ntf| {
+                                let _ = &refl::Calendar::new(ntf);
+                            });
+                        })
+                        .with_changed({
+                            let _sender = sender.clone();
+                            move |wgt| {
+                                if wgt.focus() {
+                                    let _value = wgt.value().parse::<f64>().unwrap_or_default();
+                                }
                             }
-                        }
-                    });
-            });
+                        });
+                });
             refl::Button::new(prt).set_text("4");
         });
     }
