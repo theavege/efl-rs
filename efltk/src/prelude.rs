319e6f5caf0c32c0e4e7fa7dbfcaa850b8b5f9f4
Artem V. Ageev <artem.v.ageev@gmail.com>
2026-06-18 18:02:07 +0300

    Add CanvasExt trait with mouse event callbacks

--- efltk/src/prelude.rs
+++ efltk/src/prelude.rs
@@ -1,7 +1,24 @@
 
+}
 
-impl ChildExt for super::Canvas {
-    fn child(prt: &impl ContainerExt) -> Self {
-        Self::new(prt)
+pub trait CanvasExt: WidgetExt {
+    fn with_mouse_down_left<F: FnMut(Self) + 'static>(self, func: F) -> Self {
+        self.set_callback(MouseSignal::DownLeft, func);
+        self
+    }
+    fn with_mouse_down_right<F: FnMut(Self) + 'static>(self, func: F) -> Self {
+        self.set_callback(MouseSignal::DownRight, func);
+        self
+    }
+    fn with_mouse_up_left<F: FnMut(Self) + 'static>(self, func: F) -> Self {
+        self.set_callback(MouseSignal::UpLeft, func);
+        self
+    }
+    fn with_mouse_move<F: FnMut(Self) + 'static>(self, func: F) -> Self {
+        self.set_callback(MouseSignal::Move, func);
+        self
     }
 }
+
+impl CanvasExt for super::Canvas {}
+
