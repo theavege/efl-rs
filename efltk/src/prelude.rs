125caed4db9059c497cf6e6d7c682f40fa7fcf2b
Artem V. Ageev <artem.v.ageev@gmail.com>
2026-06-18 18:00:44 +0300

    Add Canvas widget to prelude

--- efltk/src/prelude.rs
+++ efltk/src/prelude.rs
@@ -1,21 +1,7 @@
 
 
-#[derive(Default)]
-pub enum MouseSignal {
-    #[default]
-    DownLeft,
-    DownRight,
-    UpLeft,
-    Move,
-}
-
-impl SignalExt for MouseSignal {
-    fn to_str(&self) -> &str {
-        match self {
-            Self::DownLeft => "mouse,down,1",
-            Self::DownRight => "mouse,down,3",
-            Self::UpLeft => "mouse,up,1",
-            Self::Move => "mouse,move",
-        }
+impl ChildExt for super::Canvas {
+    fn child(prt: &impl ContainerExt) -> Self {
+        Self::new(prt)
     }
 }
